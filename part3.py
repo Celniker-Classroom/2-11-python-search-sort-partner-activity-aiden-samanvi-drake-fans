b399a8ad5409de60bb677ec01738b944ac4ed72b
Samanv1 <Jonnalagaddas6148@my.sduhsd.net>
2026-02-19 17:42:56 +0000

    part3a

---
+++ part3.py
@@ -0,0 +1,29 @@
+from random import randint #This line imports the randint function from the random module. The randint function generates a random integer between two specified values
+
+random = randint(1,20)
+ranNums = [] #name your list and make sure it is empty!
+userInput = int(input("Choose a number to search for: "))
+
+# Generates a list of 5 or 10 random integers between 1 and 50 inclusive.
+for index in range(10): #for loop appends 5 numbers to your list, but make sure you name your variable
+    ranNums.append(randint(1,50)) #this adds a random number between 1-50 to the list
+
+
+print("Generated list:", ranNums) #print the list!
+print("Searching for number:", userInput)
+
+
+comparisons = 0  # Initialize the counter for comparisons
+found = False  # Variable to track if the number was found
+
+
+for num in ranNums:  # Name your variable in the for loop
+    comparisons += 1  # Increment the counter for each comparison
+    if userInput == num:
+        found = True  # Set found to True if the number is in the list
+        break  # Exit the loop early if the number is found
+
+if userInput in ranNums:
+    print("Number",userInput,"found in the list after", comparisons, "comparisons!")
+else:
+    print("Number",userInput,"not found in the list after", comparisons, "comparisons!")
