c40efa2516ad882bb90907749228501ea8f782b2
parkparkceo <parka5610@my.sduhsd.net>
2026-02-19 17:48:39 +0000

    part3b

---
+++ part3b.py
@@ -0,0 +1,12 @@
+from random import randint #This line imports the randint function from the random module. The randint function generates a random integer between two specified values
+
+random = randint(1,20)
+ranNums = [] #name your list and make sure it is empty!
+
+# Generates a list of 5 or 10 random integers between 1 and 50 inclusive.
+for index in range(10): #for loop appends 5 numbers to your list, but make sure you name your variable
+    ranNums.append(randint(1,50)) #this adds a random number between 1-50 to the list
+
+
+print("Generated list:", ranNums) #print the list!
+print("The smallest value in the list is", min(ranNums))
